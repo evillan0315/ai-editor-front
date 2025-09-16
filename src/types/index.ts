0d4d238e2b72850a3aaecad1bad6212a130092ce
Eddie Villanueva <evillan0315@gmail.com>
2025-09-17 04:38:27 +0800

    added project.ts in types. Update types/index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,9 +1,4 @@
-
 // /media/eddie/Data/projects/nestJS/nest-modules/project-board-server/apps/project-board-front/src/types/index.ts
-import { AlertColor } from '@mui/material';
-
-
-
 // =========================================================================
 // Core Type Exports from Refactored Modules
 // =========================================================================
@@ -14,14 +9,4 @@ export * from './refactored/spotify';
 export * from './terminal';
 export * from './auth';
 export * from './resume';
-
-// =========================================================================
-// Transcription Types (Re-exported from media for convenience)
-// =========================================================================
-/*export {
-  TranscriptionSegment,
-  TranscriptionResult,
-  SyncTranscriptionRequest,
-  SyncTranscriptionResponse,
-  TranscriptionApi,
-} from './refactored/media';*/
+export * from './project';
