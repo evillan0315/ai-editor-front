f783ee4595af924fa857d53b54130c346d8415f5
Eddie Villanueva <evillan0315@gmail.com>
2025-10-25 01:57:08 +0800

    feat: add OpenVidu session connection APIs

---
+++ src/components/swingers/api/connections.ts
@@ -0,0 +1,4 @@
+import { fetchWithBasicAuth, handleResponse, SLS_VIDU_URL } from '@/api/fetch';
+import { ISession } from '@/components/swingers/types';
+
+const API_SESSIONS_BASE_URL = `${SLS_VIDU_URL}/api/sessions`;
